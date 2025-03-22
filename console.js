fe151fa149e6603801520700ef7046b76968b924
swdMahfuj <swdmahfuj@gmail.com>
2025-03-22 23:06:28 +0600

    added console file

---
+++ console.js
@@ -0,0 +1,20 @@
+const age = 17;
+console.log(age);
+
+const booksToRead = 15;
+console.log(booksToRead);
+
+const wakeUpTime = 6;
+console.log(wakeUpTime);
+
+const wastedTime = 9;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 12;
+console.log(goals);
+
+const pillow = 2;
+console.log(pillow);
