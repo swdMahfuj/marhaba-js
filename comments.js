4237a3bad31e2224b219a328c17fbb7a2d0de900
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 23:05:11 +0600

    added comments.js file

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Football: Germany scored 7 goals against Brazil
+const score = 7;
+
+// Declare a variable 'radius' to store the circle's radius
+// This value represents the distance from the center to the edge
+// You can change the radius to calculate a different area
+let radius = 6;
+
+// Declare a variable 'dress' to store the number of favorite dresses
+/*
+ 1. "T-Shirt" is comfortable for summer.
+ 2. "Jacket" is comfortable for winter.
+ 3. "Hoodie" is cozy and great for casual wear. 
+*/
+const dress = 3;
+
+// Store the gifted money in a variable
+const giftMoney = 100;
+/*
+This money was given as a gift for my birthday.
+*/
+
+// 1. Math-2 hours (Algebra, Geometry, Calculus)
+// 2. Programming - 2 hours (Js, Problem Solving)
+// 3. English - 1.5 hours (Grammar, Writing, Vocabulary)
+// 4. Science - 1.5 hours (Physics, Chemistry, Biology)
+const studyTime = 7;
+
+// The sendMoney variable stores the total amount of 1000 tk to be sent.
+// Service change is 14.90 tk.
+const sendMoney = 1000;
+
+// Total amount including product price and delivery fee: 1200 tk(Product price: 1000 tk, Delivery charge: 200 tk)
+const pay = 1200;
