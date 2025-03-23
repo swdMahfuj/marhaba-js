d3cef2f6a30923e40754cbb7fa495a8fabee4a1a
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 22:21:09 +0600

    added let.js file

---
+++ let.js
@@ -0,0 +1,25 @@
+let age = 17;
+console.log(age);
+age = 27;
+console.log(age);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+let playTime = 3;
+playTime = 1;
+console.log(playTime);
