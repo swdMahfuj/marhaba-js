a0eaa231932c8a2de80f42b8759a826cc6212eaf
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 21:14:05 +0600

    added true-false.js file

---
+++ true-false.js
@@ -0,0 +1,24 @@
+const friendHappy = false;
+console.log(friendHappy);
+
+const crushMessage = true;
+console.log(crushMessage);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
