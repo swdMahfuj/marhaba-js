ed4e47e780984bda3dcc273453845a3127f93140
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 20:59:50 +0600

    added string.js file

---
+++ string.js
@@ -0,0 +1,20 @@
+const name = "Mahfuj";
+console.log(name);
+
+const address = "Kalaroa, Satkhira"
+console.log(address);
+
+const college = "Kalaroa Govt. College";
+console.log(college);
+
+const fan = "Priyanka Chopra";
+console.log(fan);
+
+const movieOfTheYear = "Pushpa 2: The Rule";
+console.log(movieOfTheYear);
+
+const hobby = "video game/movie";
+console.log(hobby);
+
+const color = "black/red";
+console.log(color);
