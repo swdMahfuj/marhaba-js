759c56f3ac0b00803f9aad4855f8d002938caa95
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 22:10:05 +0600

    added type.js file

---
+++ type.js
@@ -0,0 +1,18 @@
+const price = 555;
+console.log(typeof price);
+
+const word = "learning";
+console.log(word);
+console.log(typeof word);
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+const temperature = 102.5;
+console.log(typeof temperature);
+
+const isHungry = true;
+console.log(typeof isHungry);
+
+const isRich = true;
+console.log(typeof isRich);
