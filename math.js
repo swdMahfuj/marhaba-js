57785f35ef7595b93a3b8fd86de2f3da0871740e
swdMahfuj <swdmahfuj@gmail.com>
2025-03-23 22:02:21 +0600

    added math.js file

---
+++ math.js
@@ -0,0 +1,55 @@
+const আয় = 100;
+const খরচ = 40;
+const বাকি = আয় - খরচ;
+console.log(বাকি);
+
+const পেন্সিল = 10;
+const ব্যবহার = 2;
+const মোট_ব্যবহার = পেন্সিল / ব্যবহার;
+console.log(মোট_ব্যবহার);
+
+const totalMoney = 60;
+const expensePerTime = 15;
+const timesSpent = totalMoney / expensePerTime;
+const remainingMoney = totalMoney % expensePerTime;
+console.log(timesSpent);
+console.log(remainingMoney);
+
+const first = "Hello";
+const second = "World";
+console.log(first + second);
+
+const riceWeight = 153;
+const lentilsWeight = 261;
+const totalWeight = riceWeight + lentilsWeight;
+console.log(totalWeight);
+
+const money = 500;
+const expensePerTime2 = 75;
+const timesSpent2 = money / expensePerTime2;
+const remainingMoney2 = money % expensePerTime2;
+console.log(timesSpent2) ;
+console.log(remainingMoney2);
+
+const number = 8;
+const divisor = 3;
+const result = number / divisor;
+const remainder = number % divisor;
+console.log(result);
+console.log(remainder);
+
+const num = 50;
+const divisor2 = 9;
+const remainder2 = num % divisor2;
+console.log(remainder2);
+
+const string1 = "Bangla";
+const string2 = "desh";
+console.log(string1 + string2);
+
+const totalRice = 480;
+const eatenDaily = 4;
+const day = totalRice / eatenDaily;
+const month = day / 30;
+console.log(day);
+console.log(month);
