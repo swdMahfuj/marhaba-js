a41ca204cd7a3c4b9ab24f360ea068c7d551693c
swdMahfuj <swdmahfuj@gmail.com>
2025-03-24 11:38:18 +0600

    added parse.js file

---
+++ Variable/parse.js
@@ -0,0 +1,36 @@
+const input = "20";
+const number = parseInt(input);
+const sum = number + 10;
+console.log(sum);
+
+const fraction = '3.14159';
+const convertedNumber = parseInt(fraction);
+console.log(convertedNumber);
+
+const string = 'premikBoy';
+const convertedResult = parseInt(string);
+console.log(convertedResult);
+
+const num1 = 3.456;
+const num2 = 2.789;
+const total = (num1 + num2);
+const totalNumber = parseFloat(total.toFixed(2));
+console.log(totalNumber);
+
+const num3 = '56.78';
+const num4 = '12.34';
+const result = parseFloat(num3) + parseFloat(num4);
+console.log(result);
+
+const number1 = '10.5678';
+const number2 = '5.4321';
+const convertedString1 = parseFloat(number1);
+const convertedString2 = parseFloat(number2);
+const convertedNumber1 = parseFloat(convertedString1.toFixed(1));
+const convertedNumber2 = parseFloat(convertedString2.toFixed(1));
+console.log(convertedNumber1);
+console.log(convertedNumber2);
+
+const Input = '42.45689754';
+const Number = parseFloat(Input);
+console.log(Number.toFixed(3));
