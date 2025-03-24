b279fbb23333d50b091d3983a78374ac52049f02
swdMahfuj <swdmahfuj@gmail.com>
2025-03-24 11:58:16 +0600

    added comparison-1.js file

---
+++ Comparison/comparison-1.js
@@ -0,0 +1,27 @@
+const me = 7;
+const brother = 5;
+console.log(me > brother);
+
+const myClassStudents = 45;
+const anotherClassStudents = 50;
+console.log(myClassStudents > anotherClassStudents);
+
+const myMarks = 75;
+const friendMarks = 75;
+console.log(myMarks == friendMarks);
+
+const money = 95;
+const target = 100;
+console.log(money >= target);
+
+const num1 = 10;
+const num2 = 7;
+console.log(num1 != num2);
+
+const number1 = 20;
+const number2 = 15;
+console.log(number1 <= number2);
+
+const myMarks1 = 45;
+const friendMarks1 = 97;
+console.log(myMarks1 <= friendMarks1);
