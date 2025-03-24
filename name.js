98393d05de7af43963ea6cfb95b233a2d48a0c1f
swdMahfuj <swdmahfuj@gmail.com>
2025-03-24 10:52:20 +0600

    added name.js file

---
+++ name.js
@@ -0,0 +1,23 @@
+const favoriteFood = "Biriyani";
+
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName);
+console.log(username);
+
+const myAddress = "Kalaroa, Satkhira";
+
+const super = 18;
+console.log(super);
+
+const class = 11;
+console.log(class);
+
+// Variable names cannot start with a number, so it has been changed.
+const secondPosition = "Nazmul Hossain Shanto";
+console.log(secondPosition);
