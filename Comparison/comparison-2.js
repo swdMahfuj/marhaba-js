26f5f1d92bde79d9121801be2710921f68bac24f
swdMahfuj <swdmahfuj@gmail.com>
2025-03-25 09:35:55 +0600

    added comparison-2.js

---
+++ Comparison/comparison-2.js
@@ -0,0 +1,31 @@
+const myAge = 15;
+const brotherAge = 10;
+console.log(brotherAge <= myAge);
+
+const passMarks = 33;
+const myMarks = 45;
+console.log(myMarks > passMarks);
+
+const jambura = 30;
+const kodbel  = 20;
+console.log(jambura < kodbel);
+
+const myReadBooks = 10;
+const frindsReadBooks = 10;
+console.log(myReadBooks == frindsReadBooks);
+
+const myBiscuit = 15;
+const friendBiscuit = 20;
+console.log(myBiscuit < friendBiscuit);
+
+const madridGoals = 2;
+const barcaGoals = 3;
+console.log(madridGoals < barcaGoals);
+
+const myName = "Rahim";
+const friendName = "Karim";
+console.log(myName == friendName);
+
+const isMeRead = true;
+const isFriendRead  = false;
+console.log(isMeRead == isFriendRead);
