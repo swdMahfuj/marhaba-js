eaf57ae03c30ec08b808a689095b15b8f5bafca8
swdMahfuj <swdmahfuj@gmail.com>
2025-03-25 10:27:12 +0600

    added condition-1.js file

---
+++ If-else/condition-1.js
@@ -0,0 +1,47 @@
+const time = 7;
+if(time < 6){
+    console.log("You will get food");
+}
+else{
+    console.log("Jharur bari");
+}
+
+const appLogin = true;
+if(appLogin == true){
+    console.log("Welcome!");
+}
+else{
+    console.log("Get Lost!");
+}
+
+const run = true;
+if(run == true){
+    console.log("Chocolate pabe");
+}
+else{
+    console.log("Mota vuri hobe");
+}
+
+const myMarks = 79;
+if(myMarks > 80){
+    console.log("You will get bike");
+}
+else{
+    console.log("Baki pabi na");
+}
+
+const showTime = 11;
+if(showTime < 9){
+    console.log("Movie dekhbo");
+}
+else{
+    console.log("Movie dekhbo na");
+}
+
+const temperature = 17;
+if(temperature >= 30){
+    console.log("AC on kore de ");
+} 
+else{
+    console.log("kombol gaye diya ghuma");
+}
