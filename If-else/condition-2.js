2d14789e200b2f0654210293f6be057a0c6f2560
swdMahfuj <swdmahfuj@gmail.com>
2025-03-26 12:19:36 +0600

    added condition-2 file

---
+++ If-else/condition-2.js
@@ -0,0 +1,79 @@
+const age = 19;
+const height = 61;
+if(age > 18 && height > 60){
+    console.log('Gari thelbe');
+}
+else{
+    console.log('bose thakbe');
+}
+
+const mathMarks = 65;
+const englishMarks = 80;
+if(mathMarks > 80 || englishMarks > 85){
+    console.log('Vorti howar joggo');
+}
+else{
+    console.log('toke biya diya debo');
+}
+
+const GPA = 5;
+const familyIncome = 25000;
+if(GPA == 5 && familyIncome < 10000){
+    console.log('Scholarship pabe');
+}
+else{
+    console.log('Beton diya porte hobe');
+}
+
+const hisAge = 27;
+const experience = 3;
+if(hisAge < 30 && experience > 2){
+    console.log('Porikkha dita parbe');
+}
+else{
+    console.log('Porikkha dita parbe na');
+}
+
+const eggCount = 15;
+const hasHen = true;
+if(eggCount > 12 || hasHen == false) {
+    console.log('Korma ranna  korbi');
+}
+else{
+    console.log('Bread r banana khabi');
+}
+
+const temperature = 98;
+const cough = false;
+if(temperature > 100 || cough == true){
+    console.log("Doctor er kase jabe");
+}
+else{
+    console.log("katha muri diya suye thakbe");
+}
+
+const attendance = 65;
+const submittedHomework = true;
+if(attendance > 80 && submittedHomework != false){
+    console.log('Exam dita deya hobe');
+}
+else{
+    console.log("Auto fail");
+}
+
+
+const current = true;
+const charge = false;
+if(current == false && charge == false){
+    console.log("Porte boshbi");
+}
+else{
+    console.log("Video game khelbi");
+}
+
+
+const price = 1250;
+const hasCoupon = true;
+if(price > 1000 && hasCoupon == true){
+    console.log('20% discount deya hobe')
+}
